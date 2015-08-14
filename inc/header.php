bb2c82237b1bd40acd8b450ebf7b799bde9fed02
Muhammet Talha ŞAHİN <11541508@ogrenci.firat.edu.tr>
2015-08-14 23:11:59 +0300

    Bootstrap Mobile Ayarı

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LYK 2015 Php - Pr Quiz</title>
     <link rel="stylesheet" href="assets/bootstrap.min.css">
     <style>
